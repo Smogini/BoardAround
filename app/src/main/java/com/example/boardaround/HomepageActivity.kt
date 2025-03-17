3d4a7b50d60b3b73e4a983df1bb761b1ced3254b
buldra2000 <buldra00@icloud.com>
2025-03-17 21:13:19 +0100

    RegisterActivity

--- app/src/main/java/com/example/boardaround/HomepageActivity.kt
+++ app/src/main/java/com/example/boardaround/HomepageActivity.kt
@@ -1,10 +1,19 @@
 package com.example.boardaround
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import com.example.boardaround.ui.theme.BoardAroundTheme
 import com.example.boardaround.ui.theme.BoardAroundTheme
 
 class HomepageActivity : ComponentActivity() {
@@ -12,8 +21,13 @@ class HomepageActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BoardAroundTheme() {
-                Text("Benvenuto nella Homepage!")
+                HomepageScreen()
             }
         }
     }
+}
+
+@composable
+fun HomepageScreen(){
+
 }
