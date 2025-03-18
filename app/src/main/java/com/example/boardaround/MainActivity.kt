c44dfd9a0aa2c7f152c0947e6414c24ab1a837fd
buldra2000 <buldra00@icloud.com>
2025-03-18 19:36:42 +0100

    Route

--- app/src/main/java/com/example/boardaround/MainActivity.kt
+++ app/src/main/java/com/example/boardaround/MainActivity.kt
@@ -3,45 +3,21 @@ package com.example.boardaround
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.boardaround.navigation.NavGraph
 import com.example.boardaround.ui.theme.BoardAroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BoardAroundTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // Qui chiamiamo la funzione NavGraph per gestire la navigazione
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    NavGraph() // Questo è il punto dove la NavGraph viene effettivamente usata
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BoardAroundTheme {
-        Greeting("Android")
     }
 }
