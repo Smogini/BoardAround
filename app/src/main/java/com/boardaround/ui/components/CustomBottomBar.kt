daacdfd57b12a99c565f5f0da0b076ad7e865961
buldra2000 <buldra00@icloud.com>
2025-04-01 18:19:36 +0200

    Fix CustomBottomBar.kt, Fix CustomFloatingActionButton

--- app/src/main/java/com/boardaround/ui/components/CustomBottomBar.kt
+++ app/src/main/java/com/boardaround/ui/components/CustomBottomBar.kt
@@ -1,22 +1,23 @@
 package com.boardaround.ui.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.AddLocation
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.boardaround.navigation.Route
@@ -25,9 +26,24 @@ import com.boardaround.ui.theme.ButtonColor
 
 @Composable
 fun BottomBar(navController: NavController) {
+    var showExtraFabs by remember { mutableStateOf(false) }
+    val transition = updateTransition(targetState = showExtraFabs, label = "Extra Fabs Position")
+
+    val verticalOffset by transition.animateDp(
+        label = "Vertical Offset",
+        transitionSpec = { tween(durationMillis = 300, easing = EaseOutQuad) }
+    ) { visible ->
+        if (visible) (-10.dp) else 50.dp
+    }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures {
+                    showExtraFabs = false
+                }
+            }
     ) {
         BottomAppBar(
             containerColor = BottomBar,
@@ -75,13 +91,44 @@ fun BottomBar(navController: NavController) {
             }
         )
 
-        CustomFloatingActionButton(
-            onClick = {
-                /* TODO */
-            },
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .size(60.dp)
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 50.dp)
+        ) {
+
+            if (showExtraFabs) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    CustomFloatingActionButton(
+                        onClick = {
+                            navController.navigate(Route.MyProfile)
+                            showExtraFabs = false
+                        },
+                        modifier = Modifier
+                            .size(55.dp)
+                            .offset(x = -20.dp, y = verticalOffset),
+                        icon = Icons.Filled.Person
+                    )
+                    CustomFloatingActionButton(
+                        onClick = {
+                            navController.navigate(Route.NewEvent)
+                            showExtraFabs = false
+                        },
+                        modifier = Modifier
+                            .size(55.dp)
+                            .offset(x = 20.dp, y = verticalOffset),
+                        icon = Icons.Filled.AddLocation
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            CustomFloatingActionButton(
+                onClick = {
+                    showExtraFabs = !showExtraFabs
+                },
+                modifier = Modifier.size(60.dp)
+            )
+        }
     }
 }
--- app/src/main/java/com/boardaround/ui/components/CustomFloatingActionButton.kt
+++ app/src/main/java/com/boardaround/ui/components/CustomFloatingActionButton.kt
@@ -1,6 +1,5 @@
 package com.boardaround.ui.components
 
-import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
@@ -10,24 +9,24 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.boardaround.ui.theme.ButtonColor
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun CustomFloatingActionButton(
-    onClick: () -> Unit, // Azione da eseguire quando il FAB viene cliccato
-    modifier: Modifier = Modifier
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    icon: ImageVector = Icons.Filled.Add
 ) {
     FloatingActionButton(
         onClick = onClick,
-        containerColor = ButtonColor, // Colore del FAB
-        contentColor = Color.White, // Colore dell'icona del FAB
-        modifier = modifier.offset(y = (-50).dp) // Posiziona il FAB un po' più in alto, ma lascia la forma circolare
+        containerColor = ButtonColor,
+        contentColor = Color.White,
+        modifier = modifier
     ) {
-        // Icona all'interno del FAB
         Icon(
-            imageVector = Icons.Filled.Add, // Cambia l'icona se necessario
+            imageVector = icon,
             contentDescription = "+",
-            tint = Color.Black // Colore dell'icona all'interno del FAB
+            tint = Color.Black
         )
     }
 }
-
